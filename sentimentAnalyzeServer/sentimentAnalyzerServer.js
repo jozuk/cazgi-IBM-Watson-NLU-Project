a146b61f2e1a686a9b959513c7262d5ea68fb288
jozuk <35148261+jozuk@users.noreply.github.com>
2021-08-28 14:42:47 +0300

    Update sentimentAnalyzerServer.js

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,79 +1,124 @@
 const express = require('express');
 const app = new express();
+const dotenv = require('dotenv');
+dotenv.config();
 
-/*This tells the server to use the client 
-folder for all static resources*/
+function getNLUInstance() {
+    let apikey = process.env.API_KEY;
+    let apiurl = process.env.API_URL;
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2021-03-25',
+        authenticator: new IamAuthenticator({
+            apikey: apikey,
+        }),
+        serviceUrl: apiurl,
+    });
+
+    return naturalLanguageUnderstanding;
+}
+
+function sentiment(text, type){
+    const analyzeParams = {
+        'features': {
+          'sentiment': {
+            'document' : true
+          }
+        }
+    };
+    if( type == 1){
+        analyzeParams.url = text;
+    }else{
+        analyzeParams.text = text;
+    }
+    return analyzeParams;
+}
+
+function emotion(text, type){
+    const analyzeParams = {
+        'features': {
+            'emotion': {
+                'targets': [
+                    "sadness",
+                    "fear",
+                    "joy",
+                    "anger",
+                    "disgust"
+                ],
+                'document' : true
+            },
+            'keywords': {
+              'emotion': true
+            }
+        }
+    };
+    if( type == 1){
+        analyzeParams.url = text;
+    }else{
+        analyzeParams.text = text;
+    }
+    return analyzeParams;    
+}
+
 app.use(express.static('client'));
 
-/*This tells the server to allow cross origin references*/
 const cors_app = require('cors');
 app.use(cors_app());
 
-/*Uncomment the following lines to loan the environment 
-variables that you set up in the .env file*/
-
-// const dotenv = require('dotenv');
-// dotenv.config();
-
-// const api_key = process.env.API_KEY;
-// const api_url = process.env.API_URL;
-
-function getNLUInstance() {
-    /*Type the code to create the NLU instance and return it.
-    You can refer to the image in the instructions document
-    to do the same.*/
-}
-
-
-//The default endpoint for the webserver
 app.get("/",(req,res)=>{
     res.render('index.html');
   });
 
-//The endpoint for the webserver ending with /url/emotion
 app.get("/url/emotion", (req,res) => {
-    // //Extract the url passed from the client through the request object
-    // let urlToAnalyze = req.query.url
-    // const analyzeParams = 
-    //     {
-    //         "url": urlToAnalyze,
-    //         "features": {
-    //             "keywords": {
-    //                             "emotion": true,
-    //                             "limit": 1
-    //                         }
-    //         }
-    //     }
-     
-    //  const naturalLanguageUnderstanding = getNLUInstance();
-     
-    //  naturalLanguageUnderstanding.analyze(analyzeParams)
-    //  .then(analysisResults => {
-    //     //Print the JSON returned by NLU instance as a formatted string
-    //     console.log(JSON.stringify(analysisResults.result.keywords[0].emotion,null,2));
-    //     //Please refer to the image to see the order of retrieval
-    //     return res.send(analysisResults.result.keywords[0].emotion,null,2);
-    //  })
-    //  .catch(err => {
-    //  return res.send("Could not do desired operation "+err);
-    //  });
+    console.log('text emotion for url ' + JSON.stringify(req.query.url));
+    getNLUInstance().analyze(emotion(req.query.url, 1)).then(analysisResults => {
+        console.log(JSON.stringify(analysisResults, null, 2).replace("\"",''));
+        return res.send(JSON.stringify(analysisResults.result.keywords["0"].emotion));
+    })
+    .catch(err => {
+        console.log('error:', err);
+        return res.send('target not found');
+    });
 });
 
-//The endpoint for the webserver ending with /url/sentiment
 app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+    console.log('text sentiment for url ' + req.query.url);    
+    getNLUInstance().analyze(sentiment(req.query.url, 1)).then(analysisResults => {
+        console.log(JSON.stringify(analysisResults, null, 2).replace("\"",''));
+        return res.send(JSON.stringify(analysisResults.result.sentiment.document.label).replace("\"",'').replace("\"",''));
+    })
+    .catch(err => {
+        console.log('error:', err);
+        return res.send('target not found');
+    });
 });
 
-//The endpoint for the webserver ending with /text/emotion
 app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+    console.log('text emotion for ' + req.query.text);
+    getNLUInstance().analyze(emotion(req.query.text, 0)).then(analysisResults => {
+        console.log(JSON.stringify(analysisResults, null, 2).replace("\"",''));
+        return res.send(JSON.stringify(analysisResults.result.keywords["0"].emotion));
+    })
+    .catch(err => {
+        console.log('error:', err);
+        return res.send('target not found');
+    });
 });
 
 app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+    console.log('text sentiment for ' + req.query.text);    
+    getNLUInstance().analyze(sentiment(req.query.text, 0)).then(analysisResults => {
+        console.log(JSON.stringify(analysisResults, null, 2).replace("\"",''));
+        return res.send(JSON.stringify(analysisResults.result.sentiment.document.label).replace("\"",'').replace("\"",''));
+    })
+    .catch(err => {
+        console.log('error:', err);
+        return res.send('target not found');
+    });
 });
 
 let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
-
